828aaec49c3131ef3b20440bef9844ab86636353
sahreen-haider <sahreenhaider@gmail.com>
2024-03-28 15:41:18 +0530

    added args

--- model.py
+++ model.py
@@ -10,17 +10,17 @@ openai.api_key = os.environ['OPENAI_API_KEY']
 
 client = openai.OpenAI()
 
-def get_completions(prompt, model="gpt-3.5-turbo"):
+def get_completions(prompt, model="gpt-3.5-turbo", temperature=0.6, max_tokens=300):
     messages = [
-        {"role": "system", "content": "you are a chatbot who talks like a pirate"},
+        {"role": "system", "content": "you are a chatbot who talks like a pirate and answers everything with more than 1000 words"},
         {"role": "user", "content": prompt}]
     response = client.chat.completions.create(
         model=model,
         messages=messages,
-        temperature = 0
+        temperature = temperature,
+        max_tokens=max_tokens
     )
     return response.choices[0].message.content
 
-print(get_completions('what is a boat ?'))
-
+print(get_completions('write a poem about sea ?', temperature=0.9, max_tokens=1000))
 
